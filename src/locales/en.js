4660cb8ed3206383bd51e5400ca8a357937390da
msramalho <19508417+msramalho@users.noreply.github.com>
2023-05-25 12:55:25 +0100

    new description with translation

--- src/locales/en.js
+++ src/locales/en.js
@@ -13,30 +13,37 @@ export default {
     title: `Tajikistan-Kyrgyzstan Border Clash 2022`,
     panel: {
       title: `Tajikistan-Kyrgyzstan Border Clash 2022`,
-      card: `This database was created by Miguel Ramalho as part of Bellingcat’s Tech Team using research gathered by Aiganysh Aidarbekova and Narine Khachatryan using available open source evidence, including media and social media footage.
+      card: `This map was created by Miguel Ramalho from Bellingcat’s Investigative Tech Team using research gathered by Aiganysh Aidarbekova and Narine Khachatryan from available open source evidence, including video and social media footage.
 
-      It shows changes to buildings observed via satellite imagery following clashes between Tajikistan and Kyrgyzstan between September 14 and September 19, 2022. Where possible, we have geolocated buildings using available social media and media footage. This forms the first layer of the map. It should be noted there is very limited social media and media content posted from inside Tajikistan — a country that <a target="_blank" href="https://rsf.org/en/country/tajikistan">heavily restricts</a> the media and social media usage. The second layer of the map we have identified changes to buildings in both high resolution satellite imagery and PlanetScope imagery, but further investigation is needed. The changes to buildings observed on satellite imagery does not provide conclusive proof of damage caused during the conflict - but provides a starting point for investigation for researchers interested to see areas of potential impact from the conflict. This is not a living document but is designed to provide a database for researchers and interested parties.
+It shows changes to buildings observed via satellite imagery following clashes between Tajikistan and Kyrgyzstan between September 14 and September 19, 2022. These changes could reflect damage from the clashes.
 
-      <h3>A Note on the Disputed Border</h3>
-      Sections of the border between Tajikistan and Kyrgyzstan are not conclusively demarcated, and may appear differently on various maps. We have used OpenStreetMaps as the base layer for the map — it includes a border but we can not vouch for its accuracy and recognise that it may be disputed in some places. Where we have marked incidents we have referred to the village’s name based on Yandex Maps.
+The <span class="geolocated">Blue</span> icons represent all the buildings we have geolocated using available social media and other media footage, as well as satellite imagery from Planet and Google Earth Pro. Videos and images are listed and visible when users click on these icons. The <span class="satellite">Green</span> icons represent all buildings we have geolocated and noticed changes using just satellite imagery. The changes to buildings observed on satellite imagery does not provide conclusive proof of damage caused during the clashes, but it provides a starting point for investigation by researchers interested in analysing areas of potential impact from the clashes. Though not included in this map, we also looked at PlanetScope Scene imagery from Planet Labs which provides lower resolution but more frequent images of the area and helped to further establish that the change to buildings occurred during the clashes. This map is not a living document but is designed to provide a database for researchers and interested parties.
 
-      <h3>A Note on Verification</h3>
-      The locations of the incidents mapped out in this project have been identified via satellite imagery. Using Google Earth Pro, NASA FIRMS, Planet Labs satellite imagery and social media and media footage, Bellingcat and Global Authentication Project (GAP) volunteers surveyed areas of interest from the recent clashes, highlighting areas where changes to buildings can be observed on satellite imagery.  It should be noted that satellite images show change to a building. As open sources and satellite imagery can’t tell us what exactly happened at each site, we cannot confirm that the damage was caused by the recent conflict nor who was responsible for it. Therefore, this map is not an exhaustive list, but highlights areas of interest for researchers and others looking into the impacts of the Kyrgyzstan-Tajikistan clash.
+Because the social media footage embedded is open source, the users who uploaded the content are not affiliated to Bellingcat. Any opinions that may be contained within the posts are therefore not those of Bellingcat. Any claims contained within the posts have also not necessarily been confirmed or verified by Bellingcat, particularly in relation to which party may have been responsible for the incidents detailed. It should be noted there is very limited social media and other media content posted from inside Tajikistan — a country that <a target="_blank" href="https://www.codastory.com/disinformation/kyrgyzstan-tajikistan-border-conflict/">heavily restricts</a> the media and social media usage.
 
-      In the map’s first layer we have included those incidents that Bellingcat has geolocated, using  social media and news reports.
+<h3>A Note on the Disputed Border</h3>
 
-      <h3>A Note on the Descriptions and Filters</h3>
-      Each larger circle on the map represents a geographic grouping of incidents. Every building is represented by a symbol on the map.
+Sections of the border between Tajikistan and Kyrgyzstan are not conclusively demarcated, and may appear differently on various maps. We have used OpenStreetMaps as the base layer for this map — it includes a border but we can not vouch for its accuracy and recognise that it may be disputed in some places. Similarly when we have marked incidents we have used the village name and country that appears on Yandex Maps, but recognise that some of these designations may be disputed.
 
-      “Geolocated” incidents appear in <span class="geolocated">Blue</span>. Each “Geolocated” incident includes coordinates, satellite imagery of before and after and either footage or stills from media or social media.These incidents have been geolocated by Bellingcat.
+<h3>A Note on Verification</h3>
 
-      “Satellite” incidents appear in <span class="satellite">Green</span>. Each “Satellite” incident includes coordinates as well as satellite imagery of before and after.These are incidents where change to building has been observed on satellite imagery, but further investigation is needed.
+The locations of the incidents mapped out in this project have been identified via satellite imagery. Using Google Earth Pro, NASA FIRMS, Planet Labs satellite imagery and social media and other video footage, Bellingcat and its Global Authentication Project (GAP) volunteers surveyed areas of interest from the recent clashes, highlighting areas where changes to buildings can be observed on satellite imagery. As open sources and satellite imagery can’t tell us what exactly happened at each site, we cannot confirm that the damage was caused by the 2022 clashes nor who was responsible for it. Therefore, this map is not an exhaustive list, but highlights areas of interest for researchers and others looking into possible impact of the 2022 incidents.
 
-      The coordinates match the exact building. All incidents can be seen in satellite imagery. In four cases we have used Planet Labs Images- as they provided high quality images of areas immediately after the clashes occurred, in all other cases we used Google Earth Pro satellite images which provided good quality images in the days and weeks after fighting occurred.Satellite imagery that we have used was taken up to six weeks after the clashes or up to a year before them.
+<h3>A Note on the Descriptions and Filters</h3>
 
-      At the bottom of the map, a user can toggle between incidents that apparently involve Civilian Infrastructure; Private Property (Private Homes and Businesses) or Border Posts. Decisions on these classifications are based on available open source evidence for the function of these buildings.
+Each larger circle on the map represents a geographic grouping of incidents. Every building that has changed is represented by a symbol on the map.
 
-      <div><small><a class="text-right" target="_blank" href="https://www.bellingcat.com/about/data-privacy/">Privacy policy.</a></small></div>
+<span class="geolocated">“Satellite Imagery with Media”</span> incidents appear in <span class="geolocated">Blue</span>. Each “With Media” incident includes coordinates, satellite imagery of before and after and either footage or stills from media or social media.
+
+<span class="satellite">“Satellite Imagery without Media”</span> incidents appear in <span class="satellite">Green</span>. Each “Without Media” incident includes coordinates as well as satellite imagery of before and after the clashes.
+
+The coordinates we have provided match the exact building, however the icons included in the map may appear slightly to the side of the building so that it can be clearly viewed . All incidents can be seen in satellite imagery. In five cases, we have used Planet Labs Skysat Images, in all other cases we used images displayed on Google Earth Pro.
+
+At the bottom of the map, a user can toggle between incidents that appear to involve Civilian Infrastructure or Private Property (Private Homes and Businesses). Decisions on these classifications are based on available open source evidence for the function of these buildings.
+
+Once loaded, you can click on a satellite image to toggle between before and after imagery.
+
+<div><small><a class="text-right" target="_blank" href="https://www.bellingcat.com/about/data-privacy/">Privacy policy.</a></small></div>
       `,
       dataBreakdown: `Data breakdown`,
       table: {
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -9,34 +9,58 @@ export default {
   $vuetify: {
     badge: ""
   },
+  notes: `
+
+  <span class="geolocated">Blue</span>
+
+  <span class="satellite">Green</span>
+
+  <div><small><a class="text-right" target="_blank" href="https://www.bellingcat.com/about/data-privacy/">Privacy policy.</a></small></div>
+  `,
   main: {
-    title: `Tajikistan-Kyrgyzstan Border Clash 2022`,
+    title: `Столкновение на границе Таджикистана и Кыргызстана в 2022 году`,
     panel: {
-      title: `Tajikistan-Kyrgyzstan Border Clash 2022`,
-      card: `This database was created by Miguel Ramalho as part of Bellingcat’s Tech Team using research gathered by Aiganysh Aidarbekova and Narine Khachatryan using available open source evidence, including media and social media footage.
+      title: `Столкновение на границе Таджикистана и Кыргызстана в 2022 году`,
+      card: `Эта карта была создана Мигелем Рамальо из технической группы расследований Беллингкэт по материалам исследований, проведенных Айганыш Айдарбековой и Нарине Хачатрян на основе доступных свидетельств из открытых источников, включая видео и кадры из социальных сетей.
 
-      It shows changes to buildings observed via satellite imagery following clashes between Tajikistan and Kyrgyzstan between September 14 and September 19, 2022. Where possible, we have geolocated buildings using available social media and media footage. This forms the first layer of the map. It should be noted there is very limited social media and media content posted from inside Tajikistan — a country that <a target="_blank" href="https://rsf.org/en/country/tajikistan">heavily restricts</a> the media and social media usage. The second layer of the map we have identified changes to buildings in both high resolution satellite imagery and PlanetScope imagery, but further investigation is needed. The changes to buildings observed on satellite imagery does not provide conclusive proof of damage caused during the conflict - but provides a starting point for investigation for researchers interested to see areas of potential impact from the conflict. This is not a living document but is designed to provide a database for researchers and interested parties.
 
-      <h3>A Note on the Disputed Border</h3>
-      Sections of the border between Tajikistan and Kyrgyzstan are not conclusively demarcated, and may appear differently on various maps. We have used OpenStreetMaps as the base layer for the map — it includes a border but we can not vouch for its accuracy and recognise that it may be disputed in some places. Where we have marked incidents we have referred to the village’s name based on Yandex Maps.
+      На ней показаны изменения в зданиях, наблюдаемые на спутниковых снимках, после столкновений между Таджикистаном и Кыргызстаном в период с 14 по 19 сентября 2022 года. Эти изменения могут отражать ущерб, нанесенный в результате столкновений.
 
-      <h3>A Note on Verification</h3>
-      The locations of the incidents mapped out in this project have been identified via satellite imagery. Using Google Earth Pro, NASA FIRMS, Planet Labs satellite imagery and social media and media footage, Bellingcat and Global Authentication Project (GAP) volunteers surveyed areas of interest from the recent clashes, highlighting areas where changes to buildings can be observed on satellite imagery.  It should be noted that satellite images show change to a building. As open sources and satellite imagery can’t tell us what exactly happened at each site, we cannot confirm that the damage was caused by the recent conflict nor who was responsible for it. Therefore, this map is not an exhaustive list, but highlights areas of interest for researchers and others looking into the impacts of the Kyrgyzstan-Tajikistan clash.
 
-      In the map’s first layer we have included those incidents that Bellingcat has geolocated, using  social media and news reports.
+      Синие значки представляют все здания, которые мы геолоцировали, используя доступные материалы из социальных сетей и других средств массовой информации, а также спутниковые снимки Planet и Google Earth Pro. Видео и изображения отображаются в списке и становятся видимыми, когда пользователи нажимают на эти значки. Зеленые значки обозначают все здания, которые мы геолоцировали и где заметили изменения, используя только спутниковые снимки. Изменения в зданиях, наблюдаемые на спутниковых снимках, не являются убедительным доказательством ущерба, причиненного во время столкновений, но они служат отправной точкой для расследования силами исследователей, заинтересованных в анализе областей потенциального воздействия столкновений. Хотя они и не включены на эту карту, мы также просмотрели снимки PlanetScope Scene от Planet Labs, которые имеют более низкое разрешение, но делаются чаще, что помогло дополнительно подтвердить, что изменения в зданиях произошли именно во время столкновений. Этот документ не обновляется после публикации, но предназначена для предоставления базы данных исследователям и заинтересованным сторонам.
 
-      <h3>A Note on the Descriptions and Filters</h3>
-      Each larger circle on the map represents a geographic grouping of incidents. Every building is represented by a symbol on the map.
 
-      “Geolocated” incidents appear in <span class="geolocated">Blue</span>. Each “Geolocated” incident includes coordinates, satellite imagery of before and after and either footage or stills from media or social media.These incidents have been geolocated by Bellingcat.
+      Поскольку видеоматериалы, размещенные в социальных сетях, являются открытыми источниками, пользователи, загрузившие контент, не связаны с Беллингкэт. Таким образом, любые мнения, которые могут содержаться в постах, не принадлежат Беллингкэт. Кроме того, любые заявления, которые содержатся в постах, не обязательно подтверждены или верифицированы Беллингкэт, особенно в отношении того, какая из сторон конфликта может нести ответственность за инцидент. Следует отметить, что существует очень ограниченное количество материалов из социальных сетей и другого медиаконтента, размещаемого из Таджикистана — страны, которая
+      <a target="_blank" href="https://www.codastory.com/disinformation/kyrgyzstan-tajikistan-border-conflict/">сильно ограничивает</a> использование СМИ и соцсетей.
 
-      “Satellite” incidents appear in <span class="satellite">Green</span>. Each “Satellite” incident includes coordinates as well as satellite imagery of before and after.These are incidents where change to building has been observed on satellite imagery, but further investigation is needed.
+      <h3>Примечание о спорной границе</h3>
 
-      The coordinates match the exact building. All incidents can be seen in satellite imagery. In four cases we have used Planet Labs Images- as they provided high quality images of areas immediately after the clashes occurred, in all other cases we used Google Earth Pro satellite images which provided good quality images in the days and weeks after fighting occurred.Satellite imagery that we have used was taken up to six weeks after the clashes or up to a year before them.
+      Участки границы между Таджикистаном и Кыргызстаном окончательно не демаркированы и могут по-разному отображаться на различных картах. Мы использовали OpenStreetMaps в качестве базового слоя для этой карты — она включает границу, но мы не можем ручаться за ее точность и признаем, что в некоторых местах она может быть оспорена. Аналогичным образом, когда мы отмечали инциденты, мы использовали название населенного пункта и страну, которые отображаются на картах Яндекса, но признаем, что некоторые из этих обозначений могут быть оспорены.
 
-      At the bottom of the map, a user can toggle between incidents that apparently involve Civilian Infrastructure; Private Property (Private Homes and Businesses) or Border Posts. Decisions on these classifications are based on available open source evidence for the function of these buildings.
 
-      <div><small><a class="text-right" target="_blank" href="https://www.bellingcat.com/about/data-privacy/">Privacy policy.</a></small></div>
+      <h3>Примечание о верификации</h3>
+
+      Места инцидентов, нанесенные на карту в рамках этого проекта, были определены с помощью спутниковых снимков. Используя спутниковые снимки Google Earth Pro, NASA FIRMS и Planet Labs, социальные сети и другие видеоматериалы, волонтеры Беллингкэт и Global Authentication Project изучили районы, представляющие интерес в связи с недавними столкновениями, выделив области, где на спутниковых снимках можно наблюдать изменения в зданиях. Поскольку открытые источники и спутниковые снимки не могут сказать нам, что именно произошло на каждом участке, мы не можем подтвердить, что ущерб был причинен столкновениями 2022 года, или определить, кто именно был ответственен за это. Таким образом, эта карта не является исчерпывающим списком, но выделяет области, представляющие интерес для исследователей и других лиц, изучающих возможные последствия инцидентов 2022 года.
+
+      <h3>Примечание по описаниям и фильтрам</h3>
+
+      Каждый большой круг на карте представляет географическую группу инцидентов. Каждое здание, которое претерпело изменения, обозначено символом на карте.
+
+
+      Инциденты «Спутниковые снимки + медиа» выделены синим цветом. Каждый инцидент «+ медиа» содержит координаты, спутниковые снимки до и после, а также видео или скриншоты из СМИ или социальных сетей.
+      Инциденты «Спутниковые снимки без медиа» выделены зеленым цветом. Каждый инцидент «без медиа» содержит координаты, а также спутниковые снимки, сделанные до и после столкновений.
+
+
+      Предоставленные нами координаты точно соответствуют зданию, однако значки, нанесенные на карту, могут располагаться немного сбоку от здания, чтобы его можно было хорошо рассмотреть. Все инциденты можно увидеть на спутниковых снимках. В пяти случаях мы использовали снимки Planet Labs Skysat, во всех остальных случаях мы использовали снимки, отображаемые в Google Earth Pro.
+
+
+      В нижней части карты пользователь может переключаться между инцидентами, которые, по-видимому, связаны с гражданской инфраструктурой или частной собственностью (частными домами и предприятиями). Решения по этим классификациям основаны на имеющихся данных из открытых источников о назначении этих зданий.
+
+
+      После загрузки вы можете переключаться между снимками «до» и «после», нажимая на спутниковый снимок.
+
+
+      <div><small><a class="text-right" target="_blank" href="https://www.bellingcat.com/about/data-privacy/">Политика конфиденциальности.</a></small></div>
       `,
       dataBreakdown: `Data breakdown`,
       table: {
@@ -48,7 +72,7 @@ export default {
   },
   cover: {
     title: `Map introduction`,
-    cover: `Using the bottom navigation rows you can go through {villageCount} Kyrgyz and Tajik villages, apply filters on the type of building affected, and method of identification used (<span class="geolocated">Satellite imagery with media</span> and <span class="satellite">Satellite imagery without media</span>). Further configurations can be found on the top-right menu.`,
+    cover: `Используя нижние навигационные строки, вы можете просмотреть 16 кыргызских и таджикских сел, применить фильтры по типу поврежденных зданий и используемому методу идентификации (<span class="geolocated">спутниковые снимки с медиа</span> и <span class="satellite">спутниковые снимки без медиа</span>). Дополнительные конфигурации можно найти в правом верхнем меню.`,
     link1: `For full methodology and context read the `,
     link2: `about page`
   },
